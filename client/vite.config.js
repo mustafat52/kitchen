8aecf30eb62a2a079c34f09af9c02bbe8fa4f2a6
MustafaT52 <mustafataj13@gmail.com>
2026-06-10 12:20:20 +0530

    some changes 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      includeAssets: ['icon-512.png'],
       manifest: {
         name: 'Kitchen Cleaning',
         short_name: 'Kitchen',
@@ -19,8 +19,8 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '897x998', type: 'image/png' },
+          { src: 'icon-512.png', sizes: 'any',     type: 'image/png', purpose: 'any maskable' },
         ],
       },
     }),
